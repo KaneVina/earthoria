afe501e3fc3a094de371c336f73a978e81b74975
KaneVina <wtskane@gmail.com>
2026-08-11 02:16:57 +0700

    fix(security): re-enable EarthoriaSecurity component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -157,7 +157,7 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      {/* <EarthoriaSecurity /> */}
+      <EarthoriaSecurity />
       <ScrollToTop />
       <CustomCursor />
       <EiraChatbox />
