c6b2c36de7601414c3c3db5a5d086211315993b5
KaneVina <wtskane@gmail.com>
2026-08-15 12:56:39 +0700

    feat(ebook-reader): add ebookPreview.css, redesign reading UI + toolbar (theme/font/1-2 page/auto-read) + ask-AI on line click

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -881,7 +881,7 @@ export default function AboutUs() {
                 <span
                   className="tech-editor-number"
                   id="tech-loc-counter"
-                  data-target="  114465 "
+                  data-target="  115030"
                 >
                   0
                 </span>
