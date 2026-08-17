17e385974516c8ecbc29b072c8943ced88ffd629
KaneVina <wtskane@gmail.com>
2026-08-17 10:22:03 +0700

    feat(loader): add traveling shine highlight to FullScreenLoader logo outline

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -881,7 +881,7 @@ export default function AboutUs() {
                 <span
                   className="tech-editor-number"
                   id="tech-loc-counter"
-                  data-target="  118176"
+                  data-target="  118296"
                 >
                   0
                 </span>
