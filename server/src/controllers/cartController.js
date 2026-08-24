9c45e01691eeb517ec5deaabb7c18433643a7782
KaneVina <wtskane@gmail.com>
2026-08-24 09:46:48 +0700

    fix(cart): sort cart items by createdAt instead of random UUID order

--- server/src/controllers/cartController.js
+++ server/src/controllers/cartController.js
@@ -4,7 +4,7 @@ const { decodeId, encodeId } = require('../utils/hashids')
 
 const CART_ITEM_INCLUDE = {
   items: {
-    orderBy: { id: 'asc' }, // TODO: đổi sang { createdAt: 'asc' } nếu CartItem có field này
+    orderBy: { createdAt: 'asc' },
     include: {
       variant: {
         include: {
